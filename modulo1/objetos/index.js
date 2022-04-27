6d88cfaf6528f73cd03412dc1c68c1ec6ecf8957
gabriel-suela <gscsuela@gmail.com>
2022-04-26 22:38:33 -0300

    resolver-exercicios-objetos

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -25,33 +25,71 @@
 
 // Questão 1
 
-//a)
-/* const pessoa = {
+
+const pessoa = {
     nome: 'Gabriel',
     apelidos: ['Suela ', 'Biel ', 'Gab ']
 }
 
-console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos}`) */
+console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos}`) 
 
-//b)
-/* const novosApelidos = [...pessoa.apelidos]
+
+ const novosApelidos = [...pessoa.apelidos]
 
 novosApelidos.push('bielen ', 'suelito ', 'cabeça ')
 
 console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${novosApelidos}`)
- */
+
 // Questão 2
+
+
+const objeto1 = {
+    nome:'Gabriel',
+    idade: 25,
+    profissao:'Programador',
+}
+
+const objeto2 = {
+    nome: 'Thays',
+    idade: 28,
+    profissao: 'Professora'
+}
+
+function entregarArray(obj1, obj2){
+    const arrayDeRetorno = [obj1.nome, obj1.nome.length, obj2.nome, obj2.nome.length, obj1.idade, obj2.idade, obj1.profissao, obj1.profissao.length, obj2.profissao, obj2.profissao.length]
+    return arrayDeRetorno
+}
+
+
+console.log(entregarArray(objeto1, objeto2))
+
 
 
 // Questão 3
 const carrinho = []
 
-const frutas = {
-    nome:[],
+const fruta1 = {
+    nome:'Abacaxi',
     disponibilidade: true
 
 }
 
-function sacolao (fruta) {
+const fruta2 = {
+    nome:'Pêra',
+    disponibilidade: false
+}
+
+const fruta3 = {
+    nome: 'Laranja',
+    disponibilidade: false
+}
+
+function sacolao (fruta){
     carrinho.push(fruta)
 }
+
+sacolao(fruta1)
+sacolao(fruta2)
+sacolao(fruta3)
+
+console.log(carrinho);
