526571b2d5ed98622f62e2e28ba38677aaddbc2e
gabriel-suela <gscsuela@gmail.com>
2022-07-05 22:23:16 -0300

    renderizacao-lista

--- modulo3/estados-insta4/src/App.js
+++ modulo3/estados-insta4/src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Post from './components/Post/Post';
-import Post2 from './components/Post2/Post2'
-import Post3 from './components/Post3/Post3'
 import './style.css'
 
 
 function App() {
+  
+  const postagens = [
+    {nome:'paulinha', img:'https://picsum.photos/50/50', msg:'https://picsum.photos/200/150'},
+
+    {nome:'gabriel', img:'https://picsum.photos/60/60', msg:'https://picsum.photos/300/250'},
+
+    {nome:'thays', img:'https://picsum.photos/40/20', msg:'https://picsum.photos/320/210'}
+  
+  ]
+
+  console.log(postagens)
+  const postsMap = postagens.map((item) => {return (
+  <Post 
+  nomeUsuario={item.nome} 
+  fotoUsuario={item.img} 
+  fotoPost={item.msg}
+  />
+
+  )})
+
 return(
   <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-          <Post2 
-          nomeUsuario={'gabriel'}
-          fotoUsuario={'https://picsum.photos/60/60'}
-          fotoPost={'https://picsum.photos/300/250'}
-          />
-          <Post3
-          nomeUsuario={'thays'}
-          fotoUsuario={'https://picsum.photos/40/20'}
-          fotoPost={'https://picsum.photos/320/210'}
-          />
+    {postsMap}
+    
   </div>
 )
 
