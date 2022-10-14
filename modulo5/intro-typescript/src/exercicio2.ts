70b6cbac3dd93fee5da8536a643ec5fd56531916
gabriel-suela <gscsuela@gmail.com>
2022-10-14 19:43:40 -0300

    exercicios concluidos

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,11 @@
+
+type Colors = string
+
+function imprimeTresCoresFavoritas() {
+    const cor1:Colors = prompt("verde")
+    const cor2:Colors = prompt("vermelho")
+    const cor3:Colors = prompt("azul")
+    console.log([cor1, cor2, cor3])
+ }
+
+ imprimeTresCoresFavoritas()
