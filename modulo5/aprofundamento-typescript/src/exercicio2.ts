91bd41d67bd4f3413b621323d6ec8cfe94a92204
gabriel-suela <gscsuela@gmail.com>
2022-10-19 22:43:12 -0300

    exercicio 2

---
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -0,0 +1,25 @@
+
+function obterEstatisticas(numeros): Estatistica{
+	maior:number,
+	menor:number,
+	media:number
+}{
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
---
+++ modulo5/aprofundamento-typescript/src/exercicio3.ts
@@ -0,0 +1,28 @@
+type Post = {
+    autor: string,
+    texto: string
+}
+
+const posts  = (:Post) => void [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
